e570685d3dcd2170720e7ac92d4e1ba690d3f7a7
GabrielTMangum <gabrieltmangum@gmail.com>
2019-10-30 16:33:15 -0400

    continuing work on getting the locat storage to work the way I want it to

--- assets/js/action.js
+++ assets/js/action.js
@@ -1,40 +1,50 @@
 
 $(document).ready(function () {
     var classes = " class = 'bg-white rounded p-3 justify-content-start mb-2'"
-    var storageId = 0
     var cityInput;
-    
+    var searchesArray = [{city: "Charlotte"}];
+
     getSearches();
-    
+
     $("#submitBtn").on("click", function () {
         cityInput = $("#cityInput").val();
-        
+
         if (cityInput === "") {
+            // console.log("hi")
             return;
         } else {
-            storeSearches (cityInput)
+            
+            storeSearches(cityInput)
             getSearches();
         }
     })
     
-    function storeSearches (x) {
+    function storeSearches() {
         
-        // $("#cityList").append("<div" + classes + ">" + x + "</div>")
-        
-        localStorage.setItem("cityInput" + storageId, x);
-        ++storageId;
-        localStorage.setItem("currentIdNumber", storageId);
-        $("#cityInput").val("");
-        
-    }
-    
-    
-    function getSearches () {
-        var searchId = JSON.parse(localStorage.getItem('currentIdNumber'))
-        console.log(searchId)
-        for(var i = 0; i<searchId; i++) {
-            var searches = localStorage.getItem("cityInput" + i)
-            $("#cityList").append("<div" + classes + ">" + searches + "</div>")
+        for(var j = 0; j < searchesArray.length + 1; j++) {
+            console.log(searchesArray[j].city)
+            if (searchesArray[j].city === cityInput) {
+                return;
+            } else {
+                searchesArray.push({ city: cityInput })
+                localStorage.setItem("searchHistory", JSON.stringify(searchesArray));
+            }
         }
+    }
+
+
+    function getSearches() {
+        var searchArray = JSON.parse(localStorage.getItem("searchHistory"));
+        if (searchArray !== null) {
+            console.log(searchArray)
+
+            for (var i = 0; i < searchArray.length; i++) {
+                $("#cityList").append("<div" + classes + ">" + "<a>" + searchArray[i].city + "</a>" + "</div>")
+            }
+
+        } else {
+            return;
+        }
+
     }
 })
