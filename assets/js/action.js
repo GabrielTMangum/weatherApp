07a6f03f289825626600fb801c8653363ad27e06
GabrielTMangum <gabrieltmangum@gmail.com>
2019-10-30 19:20:35 -0400

    succesfully built the importation of the weather data from the api

--- assets/js/action.js
+++ assets/js/action.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-    var cityInput = $("#cityInput").val()
-    var queryUrl = "api.openweathermap.org/data/2.5/weather?appid=09dd0bebd4d550456ba64ad6cd823b00&q=" + cityInput
-
-     $("#submitBtn").on("click", function getWeather () {
+    var date = moment().format('MMMM Do, YYYY')
+    
+    $("#submitBtn").on("click", function () {
+        var cityInput = $("#cityInput").val()
+        console.log(cityInput)
+        var queryUrl = ("https://api.openweathermap.org/data/2.5/weather?appid=79e9ef0c9561ec71d553efe57a221f25&units=imperial&q=" + cityInput)
 
         $.ajax({
             url: queryUrl,
             method: 'GET'
         }).then (function (response) {
 
-            $("#weatherBoxes").append("<div class= 'card'><div class = 'card-body'><h1>" + cityInput + "</h1></div></div><br>")
+            console.log(response)
+
+            $("#weatherBoxes").append("<div class= 'card'><div class = 'card-body'><h1>" + cityInput + " (" + date + ")</h1><div>Temp: " + response.main.temp + "&deg F </div><div>Humidity: " + response.main.humidity + "%</div><div>Wind Speed: " + response.wind.speed + "</div></div></div><br>")
 
         })
     })
